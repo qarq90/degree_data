93bcce2a59b3cc58e72f0ed8c50375737ef0ea53
qarq90 <scythethroughtheheart05@gmail.com>
2025-08-02 09:56:05 +0530

    DOP - 02/08/2025

---
+++ SEM 5/INP/Experiments/EXP3/EXP3D2.js
@@ -0,0 +1,13 @@
+function calculate() {
+    const n1 = 12;
+    const n2 = 4;
+    const result = `
+        Sum: ${n1 + n2} <br>
+        Difference: ${n1 - n2} <br>
+        Product: ${n1 * n2} <br>
+        Quotient: ${n1 / n2}
+      `;
+    console.log(result);
+}
+
+calculate();
